488b108adafb5aabdf81991133787214a1481234
guliuyi <glyi7@qq.com>
2018-08-01 15:27:54 +0800

    gai

--- main/main.js
+++ main/main.js
@@ -1,6 +1,6 @@
 // Write your cade below:
-module.exports = function main() {
-  var a=9,b=4,c=8,d=2
-  console.log(a%b)
-  console.log(c%d)
+module.exports = function main(a，b) {
+  var yushu=a%b;
+  return yushu;
+  
 };
